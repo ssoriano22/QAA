8ac1398fa084ef38b3997dfe69f6f2cb4d5fb2d5
Sophia Soriano <ssoriano@talapas-ln1.uoregon.edu>
2022-09-07 14:16:32 -0700

    Update to QAA.py

--- QAA_Part3/QAA.py
+++ QAA_Part3/QAA.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 
-#Bash commands used for Part 1 to process STAR alignment file. See Alignment.srun and Genome_gen.srun for STAR slurm scripts.
-#See labbook_PS8.py for full command line process.
-    #samtools view -S -b STAR_align_dre_WT_ovar12Aligned.out.sam > STAR_align_dre_WT_ovar12Aligned.out.bam
-    #samtools view STAR_align_dre_WT_ovar12Aligned.out.bam | head
-    #samtools sort STAR_align_dre_WT_ovar12Aligned.out.bam -o STAR_align_dre_WT_ovar12Aligned.sorted.bam
-    #samtools index STAR_align_dre_WT_ovar12Aligned.sorted.bam
-    #samtools view STAR_align_dre_WT_ovar12Aligned.sorted.bam 1 > STAR_align_dre_WT_ovar12Aligned_chr1.sam
-    #wc -l STAR_align_dre_WT_ovar12Aligned_chr1.sa #To count number of records in chr1 file
-
 import argparse
 
 #Define arguments to use on command line
 def get_args():
-    parser = argparse.ArgumentParser(description="A program to count mapped and unmapped primary alignments in STAR SAM output file.")
+    parser = argparse.ArgumentParser(description="A program to count mapped and unmapped primary alignments in STAR-output SAM file.")
     parser.add_argument("-f","--inputfile",help="Input file name (SAM)",type=str)
     parser.add_argument("-o","--outputfile",help="Output file name (txt)",type=str)
     return parser.parse_args()
@@ -23,7 +14,7 @@ file = args.inputfile #i.e. "STAR_align_dre_WT_ovar12Aligned.out.sam"
 output = args.outputfile #i.e. "PS8_output.txt"
 
 #Data sequencing files QAA
-# file = "/projects/bgmp/shared/2017_sequencing/demultiplexed/14_3B_control_S10_L008_R1_001.fastq.gz"
+# file = "STAR_align_mm_11_2H_both_S9_L008Aligned.out.sam"
 # file = "/projects/bgmp/shared/2017_sequencing/demultiplexed/14_3B_control_S10_L008_R2_001.fastq.gz"
 # file = "/projects/bgmp/shared/2017_sequencing/demultiplexed/11_2H_both_S9_L008_R1_001.fastq.gz"
 # file = "/projects/bgmp/shared/2017_sequencing/demultiplexed/11_2H_both_S9_L008_R2_001.fastq.gz"
@@ -50,8 +41,6 @@ with open(file,"r") as fh:
             flag = int(record.split("\t")[1]) #Assign bitwise flag as int to flag variable
             name = record.split("\t")[0] #Name of record
             reads_dict[name] = flag #Create dictionary object (name=key,flag=value)
-
-    #Dictionaries have unique keys, so multiple alignment reads will be ignored
 
     #Initialize counters for mapped and unmapped reads
     mapped_count = 0
